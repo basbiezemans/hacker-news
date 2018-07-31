5d02ac85575e381df934535597478ab447368476
Bas Biezemans <bas.biezemans@gmail.com>
2018-07-31 12:27:46 +0200

    Stories: Top, New, Best, Show, Ask and Jobs

--- index.php
+++ index.php
@@ -13,8 +13,9 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/views'
 ]);
 
-// Twig extensions
+// Twig globals and extensions
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
+    $twig->addGlobal('url', $_SERVER['REQUEST_URI']);
     $twig->addExtension(new VanPattenMedia\Twig\Pluralize());
     $twig->addExtension(new Twig_Extensions_Extension_Date());
     return $twig;
--- src/items.php
+++ src/items.php
@@ -5,19 +5,38 @@ use HackerNewsApi\Client\HackerNewsClient;
 
 $items = $app['controllers_factory'];
 
-$items->get('/', function () use ($app) {
+$items->get('/{type}', function ($type) use ($app) {
 
     $client = new HackerNewsClient(HackerNewsServiceClient::create());
     $page_limit = 30;
-    $list = array_slice($client->getShowStories(), 0, $page_limit);
+    
+    function stories($client, $type) {
+        switch ($type) {
+            case 'new':
+                return $client->getNewStories();
+            case 'show':
+                return $client->getShowStories();
+            case 'ask':
+                return $client->getAskStories();
+            case 'jobs':
+                return $client->getJobStories();
+            case 'best':
+                return $client->getBestStories();
+            default:
+                return $client->getTopStories();
+        }
+    }
+
+    $stories = stories($client, $type);
+    $stories = array_slice($stories, 0, $page_limit);
     $items = [];
     
-    foreach ($list as $id) {
+    foreach ($stories as $id) {
         $items[] = $client->getItem($id);
     }
     
     return $app['twig']->render('items.html.twig', ['items' => $items]);
     
-})->bind('items');
+})->bind('items')->value('type', 'top');
 
 return $items;
