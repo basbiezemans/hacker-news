223e8ff1a88c725575a2a517b83bb47194286032
Bas Biezemans <bas.biezemans@gmail.com>
2023-09-26 11:32:05 +0200

    Refactor: add parameter, code cleanup

--- src/comments.php
+++ src/comments.php
@@ -12,26 +12,20 @@ $comments->get('/', function ($item_id) use ($app) {
     
     // Returns all children of a parent node, recursively.
     // Items are retrieved from the Hacker News API 
-    function kids($parent, $level = 1) {
-        $kids = $parent->getKids();
-        if (count($kids) == 0) {
-            return [];
-        } else {
-            $client = new HackerNewsClient(HackerNewsServiceClient::create());
-            $children = [];
-            foreach ($kids as $id) {
-                $item = $client->getItem($id);
-                $dead = ($item->isDead() || $item->isDeleted());
-                if (!$dead) {
-                    $pair = [$item, $level];
-                    $children = array_merge($children, [$pair], kids($item, $level + 1));
-                }
+    function kids($parent, $client, $level = 1) {
+        $children = [];
+        foreach ($parent->getKids() as $id) {
+            $item = $client->getItem($id);
+            $dead = ($item->isDead() or $item->isDeleted());
+            if (!$dead) {
+                $pair = [$item, $level];
+                $children = [...$children, $pair, ...kids($item, $client, $level + 1)];
             }
-            return $children;
         }
+        return $children;
     }
 
-    $comments = kids($story);
+    $comments = kids($story, $client);
 
     return $app['twig']->render('comments.html.twig', ['story' => $story, 'comments' => $comments]);
     
