6807bdf1ab7dd228b9967772e53c2ace779f7f60
Bas Biezemans <bas.biezemans@gmail.com>
2023-12-03 10:12:21 +0000

    Extract function into class

--- src/HackerNews.php
+++ src/HackerNews.php
@@ -24,4 +24,30 @@ class HackerNews
             !$dead and $this->listFromTree($item, $level + 1);
         }
     }
+
+    public function stories($type, $limit = 30) {
+        $getItem = fn($id) => $this->client->getItem($id);
+        return array_map($getItem, $this->getStoryIds($type, $limit));
+    }
+
+    private function getStoryIds($type, $limit) {
+        $getStories = sprintf('get%sStories', story_type($type));
+        return array_slice($this->client->$getStories(), 0, $limit);
+    }
+}
+
+function is_valid($type) {
+    $valid = [
+        'new', 'show', 'ask',
+        'jobs', 'best', 'top',
+    ];
+    return in_array($type, $valid);
+}
+
+function story($type) {
+    return ucfirst(rtrim($type, 's'));
+}
+
+function story_type($type) {
+    return (is_valid($type) ? story($type) : 'Top');
 }
--- src/items.php
+++ src/items.php
@@ -8,32 +8,8 @@ $items = $app['controllers_factory'];
 $items->get('/{type}', function ($type) use ($app) {
 
     $client = new HackerNewsClient(HackerNewsServiceClient::create());
-    $page_limit = 30;
-    
-    function stories($client, $type) {
-        switch ($type) {
-            case 'new':
-                return $client->getNewStories();
-            case 'show':
-                return $client->getShowStories();
-            case 'ask':
-                return $client->getAskStories();
-            case 'jobs':
-                return $client->getJobStories();
-            case 'best':
-                return $client->getBestStories();
-            default:
-                return $client->getTopStories();
-        }
-    }
 
-    $stories = stories($client, $type);
-    $stories = array_slice($stories, 0, $page_limit);
-    $items = [];
-    
-    foreach ($stories as $id) {
-        $items[] = $client->getItem($id);
-    }
+    $items = (new HackerNews($client))->stories($type, 15);
     
     return $app['twig']->render('items.html.twig', ['items' => $items]);
     
